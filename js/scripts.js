f238632b5b1b77ad3235f07a2ae5979eb2cfd75b
hansashi <ahantke@mail.de>
2023-01-13 12:45:49 +0100

    created pokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,17 @@
-alert('Hello world');
-
-let favoriteFood = "apples";
-
-document.write(favoriteFood);
+let pokemonList = [
+    {
+        name: 'Bulbasur',
+        type: ['grass', 'poison'],
+        height: 0.7
+    },
+    {
+        name: 'Wartortle',
+        type: ['water'],
+        height: 1
+    },
+    {
+        name: 'Charmander',
+        type: ['fire'],
+        height: 0.6
+    }
+];
