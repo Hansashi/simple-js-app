06e57ec9f993af1ccbfb07d6272bb7970d53f0f6
hansashi <ahantke@mail.de>
2023-01-25 11:27:12 +0100

    wrapped pokemonList in an IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,34 @@
-let pokemonList = [
-    {
-        name: 'Bulbasur',
-        type: ['grass', 'poison'],
-        height: 0.7
-    },
-    {
-        name: 'Wartortle',
-        type: ['water'],
-        height: 1
-    },
-    {
-        name: 'Charmander',
-        type: ['fire'],
-        height: 0.6
+let pokemonRepository = (function () {
+    let pokemonList = [
+        {
+            name: 'Bulbasur',
+            type: ['grass', 'poison'],
+            height: 0.7
+        },
+        {
+            name: 'Wartortle',
+            type: ['water'],
+            height: 1
+        },
+        {
+            name: 'Charmander',
+            type: ['fire'],
+            height: 0.6
+        }
+    ]
+    function getAll() {
+        return pokemonList;
     }
-];
+
+    function add(item) {
+        return pokemonList.push({ name: item });
+    }
+
+    return {
+        getAll: getAll,
+        add: add
+    }
+})();
 
 pokemonList.forEach(pokemon => {
     if (pokemon.height >= 1) {
